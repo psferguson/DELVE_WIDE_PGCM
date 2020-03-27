870b27ff8ef3e0a6063829e8cb416ed055804c5a
Peter Ferguson <pferguso@des81.fnal.gov>
2020-03-27 17:25:56 -0500

    starting to debug

--- bin/DELVE_WIDE_run.py
+++ bin/DELVE_WIDE_run.py
@@ -16,7 +16,7 @@ import DELVE_WIDE_grab_refcat2 as DELVE_grab_refcat
 import DELVE_WIDE_se_objects_fnal as DELVE_se_obj
 import DELVE_WIDE_concat_se_objects_fnal as DELVE_concat_se_obj
 import DELVE_matchSortedStdObsCats as DELVE_match
-import DELVE_tie_to_stds as calc_zps
+import DELVE_tie_to_refcat2 as calc_zps
 import argparse
 
 def main():
@@ -37,23 +37,23 @@ def main():
     parser.add_argument('--BinIndex',
                       help='Index number of the Bin',
                       default='999')
-    parser.add_argument('--Config',nargs='?')
+    #parser.add_argument('--Config',nargs='?')
     args = parser.parse_args() # making it a python dict explicitly
-    if args.config:
-        config = yaml.load(open(args.config))
-    else:
-	raise ValueError('Config File Argument Required')
-    config={'do_db_query':True,
-            'do_grab_refcat2':False,
+    #if args.config:
+    #    config = yaml.load(open(args.config))
+    #else:
+    #	raise ValueError('Config File Argument Required')
+    config={'do_db_query':False,
+            'do_grab_refcat2':True,
             'do_runConvert':False,
             'do_concat':False,
             'do_match':False,
             'do_calc_zps':False,
             'prefix':"test",
-            'band_list':["g"]#["u", "g", "r", "i", "z", "Y"],
+            'band_list':["g"],#["u", "g", "r", "i", "z", "Y"],
             'verbose':1,
             #### db query  parameters
-            'query_filepaths_outfile_prefix':'DELVE_Calib_filepaths_bin',
+            'query_filepaths_outfile_prefix':'DELVE_Calib_filepaths_fnal_bin',
             'query_expinfo_outfile_prefix':'DELVE_Calib_expinfo_fnal_',
             'query_imginfo_outfile_prefix':'DELVE_Calib_imginfo_fnal_bin',
             'query_expimgfileinfo_outfile_prefix':'DELVE_Calib_expimgfileinfo_fnal_bin',
@@ -61,13 +61,13 @@ def main():
             'grab_refcat_outputFile':'ATLAS_REFCAT_2.DELVE_WIDE_',
 
             #concat se obj param
-            'concat_dir_name':'./Downloads'
+            'concat_dir_name':'./Downloads',
             #### Match config parameters
             'racolStdStarCatFile': 50,
             'deccolStdStarCatFile':2,
             'racolObsCatFile':30,
             'deccolObsCatFile':32,
-            'matchTolerance': 2.0
+            'matchTolerance': 2.0,
             #### zps calc parameters
             'fluxObsColName':'FLUX_APER_08_2',
      	    'fluxerrObsColName':'FLUXERR_APER_08_2',
@@ -81,13 +81,13 @@ def main():
     config["grab_refcat_inputFile"]=config["query_filepaths_outfile_prefix"]
 
 
-    print "prefix is "+prefix
+    #print "prefix is "+prefix
 
     # Optional command line argument is for the filter band to be used.
     # If not given, assume all filter bands (u,g,r,i,z,Y)
     # (Default value is "all", which later translates to "u g r i z Y").
     # Not all script sections below make use of BAND.
-    print "band list is "  + bandList
+    #print "band list is "  + bandList
 
 
     # 1. we call DELVE_Calib_fileimgexp_query_fnal.py
@@ -99,10 +99,10 @@ def main():
     if config["do_grab_refcat2"] == True:
             print "grabbing refcat"
             #input file= 'DELVE_Calib_filepaths_fnal_'
-            #outputfile= ’ATLAS_REFCAT_2.DELVE_WIDE_'
+            #outputfile= ATLAS_REFCAT_2.DELVE_WIDE_
             #bin index
             # verbose
-            status=DELVE_grab_refcat.do_grab_refcat2(config)
+            status=DELVE_grab_refcat.grab_relevant_refcat2_data(config)
     ############## this is where we split into filters ##############
     #################################################################
     #################################################################
@@ -115,8 +115,8 @@ def main():
 
             if os.path.isfile(inputFile)==False:
                 print """input file %s does not exist.  Exiting...""" % (inputFile)
-                return 1\
-            filepaths_file='DELVE_Calib_filepaths_fnal_'+bin_index+'.csv'
+                return 1
+            filepaths_file='DELVE_Calib_filepaths_fnal_'+config['BinIndex']+'.csv'
             df_filepaths=pd.read_csv(filepaths_file, usecols=['FILEPATH'])
             run_con_config=config.copy()
             subset=df_filepaths["FILEPATH"][df_filepaths["FILEPATH"].str[-22]==band]
@@ -134,7 +134,7 @@ def main():
             concat_config=config.copy()
 
             concat_config["concat_input_file"]=config["query_expimgfileinfo_outfile_prefix"]+concat_config["BinIndex"]+'.csv'
-            concat_config["concat_output_file"]='rawData.DELVE_Calib.'+config["band"].+config["BinIndex"]+'.csv
+            concat_config["concat_output_file"]='rawData.DELVE_Calib.'+config["band"]+'.'+config["BinIndex"]+'.csv'
 
             status=DELVE_concat_se_obj.DELVE_Calib_concat_se_objects_fnal(concat_config)
             # ../../bin/DELVE_Calib_concat_se_objects_fnal.py \
@@ -148,16 +148,16 @@ def main():
 
                 match_config["verbose"]=2
                 match_config["match_input_std_cat"]=config["grab_refcat_outputFile"]+config["BinIndex"]+'.csv'
-                match_config["match_input_obs_cat"]='rawData.DELVE_Calib.'+config["band"].+config["BinIndex"]+'.csv
-                match_config["match_output_file"]='matched-rawdata_refcat2_DEEP.'+config["band"].+config["BinIndex"]+'.csv
+                match_config["match_input_obs_cat"]='rawData.DELVE_Calib.'+config["band"]+'.'+config["BinIndex"]+'.csv'
+                match_config["match_output_file"]='matched-rawdata_refcat2_DEEP.'+config["band"]+'.'+config["BinIndex"]+'.csv'
 
                 status=DELVE_match.DELVE_matchSortedStdObsCats(match_config)
 
 
         if config["do_calc_zps"] == True:
                 calc_zps_config=config.copy()
-                calc_zps_config["zps_input_file"]='matched-rawdata_refcat2_DEEP.'+config["band"].+config["BinIndex"]+'.csv
-                calc_zps_config["zps_output_file"]='zps.matched-rawdata_refcat2_WIDE'+config["band"].+config["BinIndex"]+'.csv
+                calc_zps_config["zps_input_file"]='matched-rawdata_refcat2_DEEP.'+config["band"]+'.'+config["BinIndex"]+'.csv'
+                calc_zps_config["zps_output_file"]='zps.matched-rawdata_refcat2_WIDE'+config["band"]+'.'+config["BinIndex"]+'.csv'
                 status=DELVE_calc_zps.DELVE_tie_to_stds(calc_zps_config)
 if __name__ == "__main__":
     main()
